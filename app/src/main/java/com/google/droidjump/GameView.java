2a00ef6060d1e12b64397e7a9ee6e1fd8b70c144
Maksym Lytvynchuk <lytvynchuk.official@gmail.com>
2020-07-27 16:44:40 +0300

    Added Droid animation and ability to jump on screen click

--- app/src/main/java/com/google/droidjump/GameView.java
+++ app/src/main/java/com/google/droidjump/GameView.java
@@ -16,16 +16,145 @@
 
 package com.google.droidjump;
 
+import static androidx.navigation.Navigation.findNavController;
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class GameView extends SurfaceView implements Runnable {
 
+    private boolean isPlaying;
+    private int screenX;
+    private int screenY;
+    private SurfaceHolder surfaceHolder;
+    private MainActivity activity;
+    private Droid droid;
+    private int timePoint;
+    Thread thread = null;
+
     public GameView(Context context) {
         super(context);
+        isPlaying = false;
+    }
+
+    public GameView(Context context, int screenX, int screenY, boolean isPlaying) {
+        super(context);
+        timePoint = 0;
+        surfaceHolder = getHolder();
+        activity = (MainActivity) context;
+        this.screenX = screenX;
+        this.screenY = screenY;
+        this.isPlaying = isPlaying;
+
+        // Create droid
+        droid = new Droid(screenX / 20, screenY - screenY / 50, getResources());
+        droid.setY(droid.getY() - droid.getBitmap().getHeight());
     }
 
     @Override
     public void run() {
+        receiveLevelDetails();
+        draw();
+        while (isPlaying) {
+            update();
+            draw();
+            sleep();
+            timePoint++;
+        }
+    }
+
+    private void receiveLevelDetails() {
+        // TODO: Serialize current level data and put it in some container
+    }
+
+    public void update() {
+        // TODO: Check if time point is in level data and add data to some container, than move
+        //  it to left
+        if (droid.isJumping()) {
+            if (droid.getY() < screenY - droid.getBitmap().getHeight() - 400) {
+                droid.setJumping(false);
+            } else {
+                droid.setBitmap(droid.getDroidTypes()[4]);
+                droid.setY(droid.getY() - 100);
+            }
+        } else {
+            // Droid Animating
+            if (timePoint % 5 < 2) {
+                droid.setBitmap(droid.getDroidTypes()[5]);
+
+            } else {
+                droid.setBitmap(droid.getDroidTypes()[6]);
+            }
+        }
+        // Droid Gravity
+        if (droid.getY() != screenY - droid.getBitmap().getHeight() - (int) (screenY / 50)) {
+            droid.setY(droid.getY() + 50);
+        }
+        // Level Finishing
+        if (timePoint == 20) {
+            failGame();
+        }
+    }
+
+    public void sleep() {
+        try {
+            Thread.sleep(20);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void draw() {
+        if (surfaceHolder.getSurface().isValid()) {
+            Canvas canvas = getHolder().lockCanvas();
+            // Cleaning previous canvas
+            canvas.drawColor(Color.WHITE);
+            // Drawing droid
+            drawDroid(canvas);
+            // Drawing canvas with all elements
+            surfaceHolder.unlockCanvasAndPost(canvas);
+        }
+
+    }
+
+    public void resume() {
+        isPlaying = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    public void pause() {
+        isPlaying = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void failGame() {
+        findNavController(this).navigate(R.id.action_game_screen_to_game_failure_screen);
+    }
+
+    public void winGame() {
+        findNavController(this).navigate(R.id.action_game_screen_to_game_success_screen);
+    }
+
+    private void drawDroid(Canvas canvas) {
+        canvas.drawBitmap(droid.getBitmap(), droid.getX(), droid.getY(), null);
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (!droid.isJumping() && droid.getY() == screenY - droid.getBitmap().getHeight() - (int) (
+                screenY / 50)) {
+            droid.setJumping(true);
+        }
+        return super.onTouchEvent(event);
     }
 }
--- app/src/main/java/com/google/droidjump/MainActivity.java
+++ app/src/main/java/com/google/droidjump/MainActivity.java
@@ -18,12 +18,15 @@ package com.google.droidjump;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.main_activity);
     }
 }
