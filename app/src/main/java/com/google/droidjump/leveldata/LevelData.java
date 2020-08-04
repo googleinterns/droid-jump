37d9b5f78fad33d4765f85d9aff8e2b35bfc02aa
Daniel-ND <darya_nikolskaya@mail.ru>
2020-08-04 15:00:34 +0000

    added reading data from file

---
+++ app/src/main/java/com/google/droidjump/leveldata/LevelData.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.droidjump.leveldata;
+
+import android.content.res.Resources;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class LevelData {
+
+    public LevelData(Level level, Resources resources) throws IOException, JSONException {
+        JSONObject leveldata = new JSONObject(getJSONStringFromResource(level.fileID, resources));
+    }
+
+    private String getJSONStringFromResource(int fileID, Resources resources) throws IOException {
+        InputStream is = resources.openRawResource(fileID);
+        Scanner scanner = new Scanner(is);
+        String JSONString = scanner.useDelimiter("\\A").next();
+        is.close();
+        return JSONString;
+    }
+
+}
