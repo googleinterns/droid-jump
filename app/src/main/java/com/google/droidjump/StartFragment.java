ac3d7fde335ea5fd4c63bc6eb115ffbc78f6ec36
Maksym Lytvynchuk <lytvynchuk.official@gmail.com>
2020-08-14 16:16:18 +0300

    Updated StartFragment

--- app/src/main/java/com/google/droidjump/StartFragment.java
+++ app/src/main/java/com/google/droidjump/StartFragment.java
@@ -16,8 +16,6 @@
 
 package com.google.droidjump;
 
-import static androidx.navigation.Navigation.findNavController;
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -32,12 +30,9 @@ import com.google.droidjump.models.LevelManager;
  * Displays Start Screen.
  */
 public class StartFragment extends Fragment {
-    private MainActivity activity;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        activity = (MainActivity) getActivity();
         View rootView = inflater.inflate(R.layout.start_screen, container, /* attachToRoot= */ false);
         Button playButton = rootView.findViewById(R.id.play_button);
         Button levelButton = rootView.findViewById(R.id.level_button);
@@ -55,19 +50,22 @@ public class StartFragment extends Fragment {
     }
 
     private void play(View view) {
-        findNavController(view).navigate(R.id.action_start_screen_to_game_screen);
+        getActivity().getSupportFragmentManager().beginTransaction()
+                .replace(R.id.activity_wrapper, new GameFragment()).commit();
     }
 
     private void chooseLevel(View view) {
-        findNavController(view).navigate(R.id.action_start_screen_to_levels_screen);
+        getActivity().getSupportFragmentManager().beginTransaction()
+                .replace(R.id.activity_wrapper, new LevelsFragment()).commit();
     }
 
     private void startNewGame(View view) {
         LevelManager.resetGameData();
-        findNavController(view).navigate(R.id.action_start_screen_to_game_screen);
+        play(view);
     }
 
     private void goToHowToPlayScreen(View view) {
-        findNavController(view).navigate(R.id.action_start_screen_to_how_to_play_screen);
+        getActivity().getSupportFragmentManager().beginTransaction()
+                .replace(R.id.activity_wrapper, new HowToPlayFragment()).commit();
     }
 }
