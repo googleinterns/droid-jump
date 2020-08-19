132abb12c3baf0bd961f9e65b3ebd1507e4651c6
Maksym Lytvynchuk <lytvynchuk.official@gmail.com>
2020-08-19 15:57:47 +0300

    Added inlines in GameFailureFragment

--- app/src/main/java/com/google/droidjump/GameFailureFragment.java
+++ app/src/main/java/com/google/droidjump/GameFailureFragment.java
@@ -22,10 +22,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import androidx.fragment.app.Fragment;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 /**
  * Displays Game Failure Screen.
@@ -35,26 +33,14 @@ public class GameFailureFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.game_failure_screen,
                 container, /* attachToRoot= */ false);
-
-        // Redirecting on click to game screen.
-        FloatingActionButton retryButton = rootView.findViewById(R.id.retry_button);
-        retryButton.setOnClickListener(view -> {
+        ((LinearLayout) rootView.findViewById(R.id.droid_draw_view)).addView(new DroidStartView(getActivity()));
+        rootView.findViewById(R.id.retry_button).setOnClickListener(view -> {
             findNavController(view).navigate(R.id.action_game_failure_screen_to_game_screen);
         });
-
-        // Redirecting on click to start screen.
-        ImageButton menuButton = rootView.findViewById(R.id.menu_button);
-        menuButton.setOnClickListener(view -> {
+        rootView.findViewById(R.id.menu_button).setOnClickListener(view -> {
             findNavController(view).navigate(R.id.action_game_failure_screen_to_start_screen);
         });
-
-        // Drawing droid.
-        LinearLayout drawLayout = rootView.findViewById(R.id.droid_draw_view);
-        drawLayout.addView(new DroidStartView(getActivity()));
-
-        // Redirecting on click to How To Play screen.
-        FloatingActionButton howToPlayButton = rootView.findViewById(R.id.how_to_play_button);
-        howToPlayButton.setOnClickListener(view -> {
+        rootView.findViewById(R.id.how_to_play_button).setOnClickListener(view -> {
             findNavController(view).navigate(R.id.action_game_failure_screen_to_how_to_play_screen);
         });
         return rootView;
