8eb1ec97079007a82fa5e764bdc7ea550a3d8e4b
Maksym Lytvynchuk <lytvynchuk.official@gmail.com>
2020-08-19 09:39:05 +0300

    Fixed nit

--- app/src/main/java/com/google/droidjump/models/NavigationHelper.java
+++ app/src/main/java/com/google/droidjump/models/NavigationHelper.java
@@ -53,8 +53,7 @@ public class NavigationHelper {
 
     public static void clearBackStack(FragmentActivity activity) {
         int count = activity.getSupportFragmentManager().getBackStackEntryCount();
-        while (count > 0) {
-            count--;
+        for (int i = 0; i < count; i++) {
             activity.getSupportFragmentManager().popBackStack();
         }
     }
