b1fea89ec8a2dba929c07ae68eb585cbec43bf5e
Daniel-ND <darya_nikolskaya@mail.ru>
2020-08-19 17:10:16 +0000

    saving current code

--- app/src/main/java/com/google/droidjump/MainActivity.java
+++ app/src/main/java/com/google/droidjump/MainActivity.java
@@ -16,18 +16,119 @@
 
 package com.google.droidjump;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.droidjump.models.LevelManager;
+
 
 /**
  * Represents main activity.
  */
 public class MainActivity extends FragmentActivity {
+
+    private GoogleSignInClient mGoogleSignInClient = null;
+
+    // Request code used to invoke sign in user interactions.
+    private static final int RC_SIGN_IN = 9001;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Create the client used to sign in.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN);
+
         LevelManager.init(this);
         setContentView(R.layout.main_activity);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        signInSilently();
+    }
+
+    private void signInSilently() {
+        GoogleSignInOptions signInOptions = GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN;
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if (GoogleSignIn.hasPermissions(account, signInOptions.getScopeArray())) {
+            // Already signed in.
+            // The signed in account is stored in the 'account' variable.
+            GoogleSignInAccount signedInAccount = account;
+        } else {
+            // Haven't been signed-in before. Try the silent sign-in first.
+            GoogleSignInClient signInClient = GoogleSignIn.getClient(this, signInOptions);
+            signInClient
+                    .silentSignIn()
+                    .addOnCompleteListener(
+                            this,
+                            new OnCompleteListener<GoogleSignInAccount>() {
+                                @Override
+                                public void onComplete(@NonNull Task<GoogleSignInAccount> task) {
+                                    if (task.isSuccessful()) {
+                                        // The signed in account is stored in the task's result.
+                                        GoogleSignInAccount signedInAccount = task.getResult();
+                                        onConnected(signedInAccount);
+                                    } else {
+                                        onDisconnected();
+                                    }
+                                }
+                            });
+        }
+    }
+
+    private void onDisconnected() {
+        // Switches to main screen.
+    }
+
+    private GoogleSignInAccount mSignedInAccount = null;
+
+    private void onConnected(GoogleSignInAccount googleSignInAccount){
+        if(mSignedInAccount != googleSignInAccount){
+            mSignedInAccount = googleSignInAccount;
+
+            // Get the playerId from the PlayersClient
+            // Switch to main screen.
+        }
+    }
+
+    
+
+    private void startSignInIntent() {
+        GoogleSignInClient signInClient = GoogleSignIn.getClient(this,
+                GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN);
+        Intent intent = signInClient.getSignInIntent();
+        startActivityForResult(intent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // The signed in account is stored in the result.
+                GoogleSignInAccount signedInAccount = result.getSignInAccount();
+            } else {
+                String message = result.getStatus().getStatusMessage();
+                if (message == null || message.isEmpty()) {
+                    message = "error";
+                }
+                new AlertDialog.Builder(this).setMessage(message)
+                        .setNeutralButton(android.R.string.ok, null).show();
+            }
+        }
     }
 }
