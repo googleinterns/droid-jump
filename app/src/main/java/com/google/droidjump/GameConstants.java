a11c0de58e2758de96066b997c86ac76d137e33a
Maksym Lytvynchuk <lytvynchuk.official@gmail.com>
2020-08-27 14:42:00 +0300

    Added fetching scores functionality

--- app/src/main/java/com/google/droidjump/GameConstants.java
+++ app/src/main/java/com/google/droidjump/GameConstants.java
@@ -34,6 +34,7 @@ public interface GameConstants {
     int FIRST_LEVEL_ID = 0; // Default value.
     int INTERVAL_START_TIME = 0; // Beginning of the new time interval between obstacles.
     int NAVIGATION_START_POSITION = GravityCompat.START; // Position of navigation menu.
+    int SCORES_PER_PAGE = 25; // Count of scores on one page.
 
     double GROUND_PROPORTION = 0.6; // Handpicked value of proportion in a platform picture.
 
