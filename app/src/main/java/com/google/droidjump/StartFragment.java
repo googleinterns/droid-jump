2618d3abd3c8d9086b138bd224f3053e9436dace
Maksym Lytvynchuk <lytvynchuk.official@gmail.com>
2020-08-27 09:30:16 +0300

    Removed onResume

--- app/src/main/java/com/google/droidjump/StartFragment.java
+++ app/src/main/java/com/google/droidjump/StartFragment.java
@@ -41,11 +41,6 @@ public class StartFragment extends Fragment {
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.start_screen, container, /* attachToRoot= */ false);
