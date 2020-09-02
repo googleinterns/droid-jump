fa7802f2402aab8a35ad1c2ec5599897c0b6f24d
Daniel-ND <darya_nikolskaya@mail.ru>
2020-09-02 11:41:56 +0000

    added toast

--- app/src/main/java/com/google/droidjump/AchievementsFragment.java
+++ app/src/main/java/com/google/droidjump/AchievementsFragment.java
@@ -21,6 +21,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -62,6 +63,7 @@ public class AchievementsFragment extends Fragment {
             } else {
                 String message = Objects.requireNonNull(task.getException()).getMessage();
                 Log.e("AchievementsFragment", "Failed to load achievements from client: " + message);
+                Toast.makeText(activity, "Oops, something went wrong", Toast.LENGTH_SHORT).show();
             }
         });
         return rootView;
